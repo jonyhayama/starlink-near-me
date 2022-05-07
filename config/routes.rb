8e91f27e2fa500cbd651042474afda1b3ac745c8
Jony Hayama <jonathan.hayama@gmail.com>
2022-05-07 10:52:28 -0300

    ✨ Create `/api/near-me` endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get 'near-me', to: 'near_me#index'
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
