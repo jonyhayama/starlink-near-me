4b15b1e2cfab635ef72f4c95b6509ba24cad5036
Jony Hayama <jonathan.hayama@gmail.com>
2022-05-07 10:52:28 -0300

    ✨ Create `/api/near-me` endpoint

---
+++ app/controllers/api/near_me_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  class NearMeController < ApplicationController # rubocop:disable Style/Documentation
+    # /api/near-me?lat=40.700006352618544&lon=-74.04903955504285&qty=3
+    def index
+      @satellites = Starlink.near_me(*index_params)
+      render json: @satellites
+    end
+
+    private
+
+    def index_params
+      [params[:lat].to_f, params[:lon].to_f, params[:qty].to_i]
+    end
+  end
+end
