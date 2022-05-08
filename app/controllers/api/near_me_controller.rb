007f222d78d170df560983a8e9dc912f4252050a
Jony Hayama <jonathan.hayama@gmail.com>
2022-05-08 05:48:08 -0300

    🚚 Rename lon variable on BE

--- app/controllers/api/near_me_controller.rb
+++ app/controllers/api/near_me_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   class NearMeController < ApplicationController # rubocop:disable Style/Documentation
-    # /api/near-me?lat=40.700006352618544&lon=-74.04903955504285&qty=3
+    # /api/near-me?lat=40.700006352618544&lng=-74.04903955504285&qty=3
     def index
       @satellites = Starlink.near_me(*index_params)
       expires_in 15.minutes, public: true
@@ -11,7 +11,7 @@ module Api
     private
 
     def index_params
-      [params[:lat].to_f, params[:lon].to_f, params[:qty].to_i]
+      [params[:lat].to_f, params[:lng].to_f, params[:qty].to_i]
     end
   end
 end
